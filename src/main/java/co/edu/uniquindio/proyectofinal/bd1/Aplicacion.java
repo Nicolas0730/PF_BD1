cdf6f3e26d77a0252d5a8cb00e4760371ceb1ea9
Nicolás Jaramillo Arcos <nicolas.jaramilloa@uqvirtual.edu.co>
2023-05-18 09:37:25 -0500

    Se corrigió el error para mostrar la primera vista

--- src/main/java/co/edu/uniquindio/proyectofinal/bd1/Aplicacion.java
+++ src/main/java/co/edu/uniquindio/proyectofinal/bd1/Aplicacion.java
@@ -6,42 +6,42 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-import javax.swing.*;
-import java.awt.*;
 import java.io.IOException;
 
-public class Aplicacion  {
 
-//    private Stage primaryStage = new Stage();
-//
-//    @Override
-//    public void start(Stage primaryStage) throws IOException {
-//
-//        this.primaryStage = primaryStage;
-//        this.primaryStage.setTitle("Constructora CAMU");
-//        mostrarVentanaInicio();
-//    }
-//
-//    private void mostrarVentanaInicio() {
-//        {
-//            try {
-//                primaryStage.close();
-//                FXMLLoader loader = new FXMLLoader();
-//                loader.setLocation(Aplicacion.class.getResource("views/inicio.fxml"));
-////            loader.setController(new LoginController());
-//                AnchorPane vistaIndex = (AnchorPane) loader.load();
-//                Scene scene = new Scene(vistaIndex);
-//                primaryStage.setScene(scene);
-//                primaryStage.show();
-//            } catch (IOException e) {
-//                System.out.println(e.getMessage());
-//            }
-//        }
-//    }
+public class Aplicacion  extends Application{
+
+    private Stage primaryStage = new Stage();
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Constructora CAMU");
+        mostrarVentanaInicio();
+    }
+
+
+    private void mostrarVentanaInicio() {
+            try {
+                primaryStage.close();
+                FXMLLoader loader = new FXMLLoader();
+
+                loader.setLocation(Aplicacion.class.getResource("view/inicio.fxml"));
+                System.out.println(loader.getLocation());
+
+                AnchorPane vistaIndex = (AnchorPane) loader.load();
+                Scene scene = new Scene(vistaIndex);
+                primaryStage.setScene(scene);
+                primaryStage.show();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+    }
 
 
     public static void main(String[] args) {
-//        launch();
-        System.out.println("--------------a");
+        launch();
+//        System.out.println("--------------a");
     }
 }
